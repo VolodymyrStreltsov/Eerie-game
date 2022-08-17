fd565e69e46f807ac99756f77f3e3ac2b0653761
Vladimir <vladimir.streltsov@gmail.com>
2022-08-17 17:07:17 +0200

    start touch events

--- src/input.js
+++ src/input.js
@@ -2,8 +2,9 @@ export default class InputHandler {
     constructor(game){
       this.game = game
       this.keys = []
+      this.touchY = ''
+      this.touchTreshold = 30
       window.addEventListener('keydown', e => {
-        console.log(e.key)
         if(( e.key === 'ArrowDown' ||
              e.key === 'ArrowUp'   ||
              e.key === 'ArrowLeft' ||
@@ -12,6 +13,7 @@ export default class InputHandler {
              && this.keys.indexOf(e.key) === -1){
           this.keys.push(e.key)
         } else if(e.key === 'd') this.game.debug = !this.game.debug
+          else if(e.key === 'Enter' && this.game.gameOver) this.game.restart()
       })
       window.addEventListener('keyup', e => {
         if(  e.key === 'ArrowDown' ||
@@ -23,5 +25,19 @@ export default class InputHandler {
         }
       })
 
+      window.addEventListener('touchstart', e => {
+        this.touchY = e.changedTouches[0].clientY
+      })
+      window.addEventListener('touchmove', e => {
+        const swipeDistance = this.touchY - e.changedTouches[0].clientY
+        if(swipeDistance < -this.touchTreshold && this.keys.indexOf('ArrowDown') === -1) this.keys.push('ArrowDown')
+        else if(swipeDistance > this.touchTreshold && this.keys.indexOf('ArrowUp') === -1) this.keys.push('ArrowUp')
+      })
+      window.addEventListener('touchend', e => {
+        console.log(this.keys)
+        this.keys.splice(this.keys.indexOf('ArrowUp'), 1)
+        this.keys.splice(this.keys.indexOf('ArrowDown'), 1)
+        if(this.keys.indexOf('ArrowUp') && this.game.gameOver) this.game.restart()
+      })
   }
 }

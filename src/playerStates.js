fd1c1d195fc566083a746a37111c92cd62acdd04
Volodymyr <80418169+VolodymyrStreltsov@users.noreply.github.com>
2023-03-06 15:22:11 +0100

    Update playerStates.js

--- src/playerStates.js
+++ src/playerStates.js
@@ -29,7 +29,7 @@ export class Idle extends State {
     handleInput(input){
       if((input.includes('ArrowLeft') || input.includes('ArrowRight')) && !input.includes(' ')) this.game.player.setState(states.RUNNING, 2)
       else if(this.game.energy > 0 && input.includes(' ') && !input.includes('ArrowDown')) this.game.player.setState(states.ATTACKING, 2)
-      else if (input.includes('ArrowUp') && !input.includes('ArrowDown')) this.game.player.setState(states.JUMPING, 1)
+      else if (input.includes('ArrowUp') && !input.includes('ArrowDown')) this.game.player.setState(states.JUMPING, 2)
     }
 }
 
@@ -45,7 +45,7 @@ export class Running extends State {
     handleInput(input){
       this.game.particles.unshift(new Dust(this.game, this.game.player.x + this.game.player.width * 0.25, this.game.player.y + this.game.player.height - 10))
       if(input.includes('ArrowDown') && !(input.includes('ArrowLeft') || input.includes('ArrowRight'))) this.game.player.setState(states.IDLE, 0)
-      else if (input.includes('ArrowUp') && !input.includes('ArrowDown')) this.game.player.setState(states.JUMPING, 1)
+      else if (input.includes('ArrowUp') && !input.includes('ArrowDown')) this.game.player.setState(states.JUMPING, 2)
       else if(this.game.energy > 0 && input.includes(' ')) this.game.player.setState(states.ATTACKING, 2)
     }
 }
@@ -61,7 +61,7 @@ export class Jumping extends State {
       this.game.player.maxFrame = 14
     }
     handleInput(input){
-      if(this.game.player.vy > this.game.player.weight) this.game.player.setState(states.FALLING, 1)
+      if(this.game.player.vy > this.game.player.weight) this.game.player.setState(states.FALLING, 2)
       else if(this.game.energy > 0 && input.includes(' ')) this.game.player.setState(states.ATTACKING, 2)
       else if(this.game.energy > 0 && input.includes('ArrowDown')) this.game.player.setState(states.DIVING, 0)
     }
@@ -79,7 +79,7 @@ export class Falling extends State {
     handleInput(input){
       if(this.game.player.onGround()) this.game.player.setState(states.RUNNING, 2)
       else if(this.game.energy > 0 && input.includes('ArrowDown')) this.game.player.setState(states.DIVING, 0)
-      else if (this.game.energy > 0 && input.includes(' ')) this.game.player.setState(states.ATTACKING, 1)
+      else if (this.game.energy > 0 && input.includes(' ')) this.game.player.setState(states.ATTACKING, 2)
     }
 }
 
@@ -98,9 +98,9 @@ export class Attacking extends State {
       this.game.energy--
       this.game.particles.unshift(new Energy(this.game, this.game.player.x + this.game.player.width * 0.25, this.game.player.y + this.game.player.height * 0.5))
       if(this.game.energy < 1 && this.game.player.onGround()) this.game.player.setState(states.RUNNING, 2)
-      if(this.game.energy < 1 && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 1)
+      if(this.game.energy < 1 && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 2)
       else if(!input.includes(' ') && this.game.player.onGround()) this.game.player.setState(states.RUNNING, 2)
-      else if (!input.includes(' ') && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 1)
+      else if (!input.includes(' ') && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 2)
       else if (input.includes(' ') && this.game.player.onGround() && input.includes('ArrowUp')) this.game.player.vy -= 27
       else if(this.game.energy > 0 && input.includes('ArrowDown') && !this.game.player.onGround()) this.game.player.setState(states.DIVING, 0)
     }
@@ -143,7 +143,7 @@ export class Hit extends State {
     handleInput(input){
       if(this.game.lives > 0 && !this.game.gameOver){
       if(this.game.player.frameX >= 3 && this.game.player.onGround()) this.game.player.setState(states.RUNNING, 2)
-      else if (this.game.player.frameX >= 3 && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 1)
+      else if (this.game.player.frameX >= 3 && !this.game.player.onGround()) this.game.player.setState(states.FALLING, 2)
       }
     }
 }
